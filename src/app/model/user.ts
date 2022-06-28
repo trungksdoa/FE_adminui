c038357db9a4a83560665e40287a7ffba3c66d83
nguyencaothai <lanhoxga@gmail.com>
2022-06-28 13:01:38 +0700

    update

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -6,13 +6,17 @@ export class Users {
   public address: String
   public phone: String
   public isAdmin: boolean
+  public createAt: any;
+  public lastUpdated: any;
   constructor (
     id: number,
     name: String,
     username: String,
     password: String,
     address: String,
-    phone: String
+    phone: String,
+    createAt: any,
+   lastUpdated: any,
   ) {
     this.id = id
     this.name = name
@@ -20,5 +24,7 @@ export class Users {
     this.password = password
     this.address = address
     this.phone = phone
+    this.createAt=createAt
+    this.lastUpdated=lastUpdated
   }
 }
